8d15a46df7aff6c8e514bb33497eabea984558ac
Heiner Bunjes <heiner@bunjes.net>
2017-11-29 13:31:40 +0100

    Update add-dns.sh

--- add-dns/add-dns.sh
+++ add-dns/add-dns.sh
@@ -55,7 +55,7 @@ if [ -e $STAT_RESULT_FN ]; then
   #jq -f list-clients.jq $STAT_RESULT_FN > $LIST_CLIENTS_RESULT_#FN
 fi
 
-# -q = quiet (remove this option to identify problems
+# -q = quiet (remove this option to identify problems)
 sshpass -p "$UNIFI_PASSWD" scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ADD_DNS_RESULT_FN $UNIFI_USERNAME@$UNIFI_HOST:/srv/unifi/data/sites/default/config.gateway.json
 
 
